d09c36535da91529a2854ee22be581cec0ddca3c
Gabriel Santos <gabrielsantos1216.gs@gmail.com>
2024-07-16 14:11:19 -0300

    first commit

---
+++ bottelegran.py
@@ -0,0 +1,49 @@
+import telebot 
+from chave import CHAVE_API 
+
+
+bot = telebot.TeleBot(CHAVE_API)
+
+duplas = []
+
+# Entrada de Dados: Tela para inserir as duplas
+@bot.message_handler(commands=["opcao1"])
+def opcao1(mensagem):
+    pass
+
+
+@bot.message_handler(commands=["opcao2"])
+def opcao2(mensagem):
+    pass
+
+@bot.message_handler(commands=["opcao3"])
+def opcao3(mensagem):
+    pass
+
+@bot.message_handler(commands=["opcao4"])
+def opcao4(mensagem):
+    pass
+
+
+
+def verificar(mensagem):
+    return True
+
+
+# Tela Inicial: Escolha do método de jogo (Opção A ou B)
+@bot.message_handler(func= verificar)
+def responder(mensagem):
+    texto = """
+        Olá, eu sou o bot do Futevolei e resenha
+
+        Escolha uma opção para continuar (Clique no item):
+        /opcao1 Adicionar duplas
+        /opcao2 Sai as duas duplas 
+        /opcao3 Dupla ganhadora fica
+        /opcao4 Dupla ganhadora volta no proximo jogo
+        Responder qualquer outra coisa não vai funcionar, clique em uma das opções 
+    """
+    bot.reply_to(mensagem, texto)
+
+bot.infinity_polling()
+
